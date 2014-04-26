59a67e35d3ef7de3498d2949026a50c3eea9ed8c
SvSchmidt <wqFrAABhMHRN4S1pk8lJ>
2014-04-26 19:36:56 +0200

    First Version

--- js/jquery.animateTransform.js
+++ js/jquery.animateTransform.js
@@ -1,71 +1,54 @@
 /******************************************
- * @PluginName      animateTransform
+ * @PluginName      $.animate CSS3 Transform Hook
  *
  *
  * @author          Sven Schmidt
- * @Date            2014/04/21
+ * @Date            2014/04/26
  * @copyright       Copyright (c) 2014 Sven Schmidt
  * @license         Feel free to use!
  * @link            http://portfolio.lifesetter.de
- * @version         1.1.0
+ * @version         1.0.0 beta 1
  * @description     Use this plugin to animate objTransform3 transform-values (translate, translate3d, rotate, skew, scale)
- * @Usage           $(elem).animateTransform({x:...,y:...,z:...,skewX:...,skewY:...,rotate:...,scale:...duration:...,callback:function() {}});
+ * @Usage           $(elem).animate({transform { rotate:...; },duration,callback);
  ******************************************/
 
 (function($) {
-	$.fn.animateTransform = function(settings) {
-		var objSettings = $.extend({},$.fn.animateTransform.defaultSettings,settings || {}),
-		    vendorPrefix = function(str) { return str.charAt(0).toUpperCase() + str.slice(1); }((Array.prototype.slice.call(window.getComputedStyle(document.documentElement,"")).join("").match(/-(moz|webkit|ms)-/) || (styles.OLink === "" && ["","o"]))[1]);
+	$.fx.step["transform"] = function(fx) {
 
-		return this.each(function() {
-			var $elem = $(this),objSettings = $.extend({},$.fn.animateTransform.defaultSettings,settings || {}),
-			    x=0,y=0,z=0,rotate=0,skewX=0,skewY=0,scale=0,translateLine,rotateLine,skewLine,scaleLine,propNum = 0,
-			    objTransform = $.extend({},("    " + ($elem[0].style.WebkitTransform || $elem[0].style.MozTransform || $elem[0].style.OTransform || $elem[0].style.MSTransform || $elem[0].style.Transform || " ").replace(new RegExp("\\, ","g"),",")).split(" "),"rotate(0deg) scale(1) translate(0px,0px) skew(0deg,0deg)".replace(new RegExp("\\, ","g"),",").split(" "));	
-
+		if(!fx.myInit) {
+			fx.startTime = $.now();
+			vendorPrefix = function(str) { return str.charAt(0).toUpperCase() + str.slice(1); }((Array.prototype.slice.call(window.getComputedStyle(document.documentElement,"")).join("").match(/-(moz|webkit|ms)-/) || (styles.OLink === "" && ["","o"]))[1]),
+			$elem = $(fx.elem),x=0,y=0,z=0,rotate=0,skewX=0,skewY=0,scale=0,propNum=0,now = fx.startTime,
+			objTransform = $.extend({},("    " + ($elem[0].style.WebkitTransform || $elem[0].style.MozTransform || $elem[0].style.OTransform || $elem[0].style.MSTransform || $elem[0].style.Transform || " ").replace(new RegExp("\\, ","g"),",")).split(" "),"rotate(0deg) scale(1) translate(0px,0px) skew(0deg,0deg)".replace(new RegExp("\\, ","g"),",").split(" "));	
+		
 			for(v in objTransform) if(objTransform[v]) objTransform[objTransform[v].match(/([a-zA-Z]{0,10})/)[0].replace("3d","")] = objTransform[v].match(/[^()\s]+(?=,|\))/g)[0].replace(new RegExp("deg","g"),"").replace(new RegExp("px","g"),"").split(",") || [0,0,0];
-
-			$($elem).animate({
-				"fake7":objTransform.scale[0],
-				"fake4":objTransform.rotate[0]
-			},0);
-
-			$($elem).animate({
-				"fake1":objSettings.x,
-				"fake2":objSettings.y,
-				"fake3":objSettings.z,
-				"fake4":objSettings.rotate,
-				"fake5":objSettings.skewX,
-				"fake6":objSettings.skewY,
-				"fake7":objSettings.scale
-			},{step:function(now,fx) {
-
-				propNum = fx.prop.replace("fake","");
-
-				(propNum == 1 && (x = fx.now)) || (propNum == 2 && (y = fx.now)) || (propNum == 3 && (z = fx.now));
-				(propNum < 4 && (translateLine = "translate" + (objSettings.z > 0 ? "3d" : "") + "(" + (x) + "px," + (y) + "px" + (objSettings.z > 0 ? "," + (z) + "px)" : ")")));
-				(propNum == 4 && (rotate = fx.now) && ((objTransform.rotate[0] <= objSettings.rotate && rotate > objTransform.rotate[0]) || (objTransform.rotate[0] > objSettings.rotate && rotate <= objTransform.rotate[0])) && (rotateLine = "rotate(" + rotate + "deg)"));		 
-				(propNum == 5 && (skewX = fx.now)) || (propNum == 6 && (skewY = fx.now));
-				(propNum == (5 || 6) && (skewLine = "skew(" + skewX + "deg," + skewY + "deg)"));
-				(propNum == 7 && (scale = fx.now) && ((objTransform.scale[0] <= objSettings.scale && scale > objTransform.scale[0]) || (objTransform.scale[0] > objSettings.scale && scale <= objTransform.scale[0])) && (scaleLine = "scale(" + scale + ")"));
-
-				$elem.css(vendorPrefix + "Transform",
-					(translateLine || "") +  (rotateLine || "rotate(" + objTransform.rotate[0] + "deg)") + (skewLine || "") + (scaleLine || "scale(" + objTransform.scale[0] + ")"));
-			},duration:parseInt(objSettings.duration),complete:function() {
-					($.isFunction(objSettings.callback) && objSettings.callback.call(this));
-				}
-			});
-		});
+			
+			fx.start = {
+				scale:+objTransform.scale[0],
+				rotate:+objTransform.rotate[0],
+				skewX:+objTransform.skew[0],
+				skewY:+objTransform.skew[1],
+				x:+objTransform.translate[0],
+				y:+objTransform.translate[1],
+				z:+objTransform.translate[2]
+			};
+			
+			fx.myInit = true;
+		}
+		
+		now = $.now() - fx.startTime;
+		divisor = (now / fx.options.duration).toFixed(2);
+		rotateLine = "rotate(" + +(fx.start.rotate + ((fx.end.rotate - fx.start.rotate) * divisor)) + "deg)";
+		scaleLine = "scale(" + +(fx.start.scale + ((fx.end.scale - fx.start.scale) * divisor)) + ")";
+		
+	/*	(translateLine = "translate" + (fx.end.z > 0 ? "3d" : "") + "(" + (
+		(propNum < 4 && (translateLine = "translate" + (objSettings.z > 0 ? "3d" : "") + "(" + (x) + "px," + (y) + "px" + (objSettings.z > 0 ? "," + (z) + "px)" : ")")));
+		(propNum == 4 && (rotate = fx.now) && ((objTransform.rotate[0] <= objSettings.rotate && rotate > objTransform.rotate[0]) || (objTransform.rotate[0] > objSettings.rotate && rotate <= objTransform.rotate[0])) && (rotateLine = "rotate(" + rotate + "deg)"));		 
+		(propNum == 5 && (skewX = fx.now)) || (propNum == 6 && (skewY = fx.now));
+		(propNum == (5 || 6) && (skewLine = "skew(" + skewX + "deg," + skewY + "deg)"));
+		(propNum == 7 && (scale = fx.now) && ((objTransform.scale[0] <= objSettings.scale && scale > objTransform.scale[0]) || (objTransform.scale[0] > objSettings.scale && scale <= objTransform.scale[0])) && (scaleLine = "scale(" + scale + ")"));
+*/
+		$("#eins").css(vendorPrefix + "Transform",
+			(scaleLine || "scale(" + fx.start.scale + ")") + (rotateLine || "rotate(" + fx.start.scale + ")deg"));
 	}
-
-	$.fn.animateTransform.defaultSettings = {
-		x:0,
-		y:0,
-		z:0,
-		skewX:0,
-		skewY:0,
-		scale:1,
-		rotate:0,
-		duration:10000,
-		callback:function() {}
-	};
 })(jQuery);
