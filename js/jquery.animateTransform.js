fd211df34f8b9d2e35de846f9b2934b7a4e8f4df
SvSchmidt <wqFrAABhMHRN4S1pk8lJ>
2014-04-26 22:12:21 +0200

    Fixed some errors
    It wasn't possible to animate multiple elements at simultaneously;
    the translate-z-value was set to 0 every time because the calculation
    returned NaN.

--- js/jquery.animateTransform.js
+++ js/jquery.animateTransform.js
@@ -18,11 +18,11 @@
 
 (function($) {
 	$.fx.step["transform"] = function(fx) {
-
+		$elem = $(fx.elem);
+		
 		if(!fx.css3TransformInit) {
 			fx.startTime = $.now();
 			vendorPrefix = function(str) { return str.charAt(0).toUpperCase() + str.slice(1); }((Array.prototype.slice.call(window.getComputedStyle(document.documentElement,"")).join("").match(/-(moz|webkit|ms)-/) || (styles.OLink === "" && ["","o"]))[1]),
-			$elem = $(fx.elem),x=0,y=0,z=0,rotate=0,skewX=0,skewY=0,scale=0,propNum=0,now = fx.startTime,
 			objTransform = $.extend({},("    " + ($elem[0].style.WebkitTransform || $elem[0].style.MozTransform || $elem[0].style.OTransform || $elem[0].style.MSTransform || $elem[0].style.Transform || " ").replace(new RegExp("\\, ","g"),",")).split(" "),"rotate(0deg) scale(1) translate(0px,0px) skew(0deg,0deg)".replace(new RegExp("\\, ","g"),",").split(" "));	
 		
 			for(v in objTransform) if(objTransform[v]) objTransform[objTransform[v].match(/([a-zA-Z]{0,10})/)[0].replace("3d","")] = objTransform[v].match(/[^()\s]+(?=,|\))/g)[0].replace(new RegExp("deg","g"),"").replace(new RegExp("px","g"),"").split(",") || [0,0,0];
@@ -34,19 +34,20 @@
 				skewY:+objTransform.skew[1],
 				x:+objTransform.translate[0],
 				y:+objTransform.translate[1],
-				z:+objTransform.translate[2]
+				z:+objTransform.translate[2] || 0
 			};
-			
+
 			fx.css3TransformInit = true;
 		}
 		
 		divisor = ((Math.round(($.now() - fx.startTime) / 100) * 100) / fx.options.duration).toFixed(3);
 		rotateLine = "rotate(" + +(fx.start.rotate + ((fx.end.rotate - fx.start.rotate) * divisor)) + "deg)";
 		scaleLine = "scale(" + +(fx.start.scale + ((fx.end.scale - fx.start.scale) * divisor)) + ")";
-		translateLine = "translate" + (Math.abs(fx.end.z) > 0 ? "3d" : "") + "(" + (+(fx.start.x + ((fx.end.x - fx.start.x) * divisor)) || 0) + "px," + (+(fx.start.y + ((fx.end.y - fx.start.y) * divisor)) || 0) + "px" + (Math.abs(fx.end.z)  > 0 ? ("," + (+(fx.start.z + ((fx.end.z - fx.start-z) * divisor)) || 0) + "px") : "") + ")";
+		translateLine = "translate" + (Math.abs(fx.end.z) > 0 ? "3d" : "") + "(" + (+(fx.start.x + ((fx.end.x - fx.start.x) * divisor)) || 0) + "px," + (+(fx.start.y + ((fx.end.y - fx.start.y) * divisor)) || 0) + "px" + (Math.abs(fx.end.z)  > 0 ? ("," + (fx.start.z + ((fx.end.z - fx.start.z) * divisor) || 0) + "px") : "") + ")";
 		skewLine = "skew(" + (+(fx.start.skewX + ((fx.end.skewX - fx.start.skewX) * divisor)) || 0) + "deg," + (+(fx.start.skewY + ((fx.end.skewY - fx.start.skewY) * divisor)) || 0) + "deg)";
-		
+
 		$elem.css(vendorPrefix + "Transform",
 			(translateLine || "translate" + (fx.start.z > 0 ? "3d" : "") + (fx.start.x + "px," + fx.start.y + "px" + (fx.start.z > 0 ? fx.start.z + ",px" : "") + ")")) + (scaleLine || "scale(" + fx.start.scale + ")") + (rotateLine || "rotate(" + fx.start.scale + ")deg") + (skewLine || "skew(" + fx.start.skewX + "deg," + fx.start.skewY + "deg)"));
+	
 	}
 })(jQuery);
