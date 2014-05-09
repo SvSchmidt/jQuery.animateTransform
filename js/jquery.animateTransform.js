ffd8a9953aaf3a849941fa7e96c25c5fa7539c07
SvSchmidt <wqFrAABhMHRN4S1pk8lJ>
2014-05-09 20:43:13 +0200

    Reduced redundancy

--- js/jquery.animateTransform.js
+++ js/jquery.animateTransform.js
@@ -3,11 +3,11 @@
  *
  *
  * @author          Sven Schmidt
- * @Date            2014/04/28
+ * @Date            2014/05/09
  * @copyright       Copyright (c) 2014 Sven Schmidt
  * @license         Feel free to use!
  * @link            http://portfolio.lifesetter.de
- * @version         2.1.0 
+ * @version         2.2.0
  * @description     Use this plugin to animate CSS3 Transform values (translate, translate3d, rotate, skew, scale)
  * @Usage           $(elem).animate({
  *                      something:somevalue,
@@ -19,13 +19,11 @@
 
 (function($) {
 	$.fx.step["transform"] = function(fx) {
-		$elem = $(fx.elem);
-		
 		if(!fx.css3TransformInit) {
-			fx.startTime = $.now();
-			vendorPrefix = function(str) { return str.charAt(0).toUpperCase() + str.slice(1); }((Array.prototype.slice.call(window.getComputedStyle(document.documentElement,"")).join("").match(/-(moz|webkit|ms)-/) || (styles.OLink === "" && ["","o"]))[1]),
-			objTransform = $.extend({},("    " + ($elem[0].style.WebkitTransform || $elem[0].style.MozTransform || $elem[0].style.OTransform || $elem[0].style.MSTransform || $elem[0].style.Transform || " ").replace(new RegExp("\\, ","g"),",")).split(" "),"rotate(0deg) scale(1) translate(0px,0px) skew(0deg,0deg)".replace(new RegExp("\\, ","g"),",").split(" "));	
-		
+			fx.$elem = $(fx.elem),fx.startTime = $.now(),
+			vendorPrefix = (typeof vendorPrefix === "undefined" ? function(str) { return str.charAt(0).toUpperCase() + str.slice(1); }((Array.prototype.slice.call(window.getComputedStyle(document.documentElement,"")).join("").match(/-(moz|webkit|ms)-/) || (styles.OLink === "" && ["","o"]))[1]) : vendorPrefix),
+			objTransform = $.extend({},("    " + (fx.$elem[0].style.WebkitTransform || fx.$elem[0].style.MozTransform || fx.$elem[0].style.OTransform || fx.$elem[0].style.MSTransform || fx.$elem[0].style.Transform || " ").replace(new RegExp("\\, ","g"),",")).split(" "),"rotate(0deg) scale(1) translate(0px,0px) skew(0deg,0deg)".replace(new RegExp("\\, ","g"),",").split(" "));	
+
 			for(v in objTransform) if(objTransform[v]) objTransform[objTransform[v].match(/([a-zA-Z]{0,10})/)[0].replace("3d","")] = objTransform[v].match(/[^()\s]+(?=,|\))/g)[0].replace(new RegExp("deg","g"),"").replace(new RegExp("px","g"),"").split(",") || [0,0,0];
 			
 			fx.start = {
@@ -58,7 +56,7 @@
 		
 		}
 
-		$elem.css(vendorPrefix + "Transform",
+		fx.$elem.css(vendorPrefix + "Transform",
 			("translate" + (fx.now.translate.bIs3d ? "3d" : "") + "(" + fx.now.translate.x + "px," + fx.now.translate.y + "px" + (fx.now.translate.bIs3d ? ("," + fx.now.translate.z + "px") : "") + ")") +
 			("rotate(" + (fx.now.rotate) + "deg)") +
 			("scale(" + (fx.now.scale) + ")") +
